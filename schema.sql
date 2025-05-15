95b3ef15561f493c9fde150d68002019ad123b81
Jordi Hamberg <150976769+jordihamberg@users.noreply.github.com>
2025-05-15 16:57:42 +0200

    Final

--- schema.sql
+++ schema.sql
@@ -1,128 +1,133 @@
+DROP SCHEMA IF EXISTS trabajo;
 -- 1. Crear el schema y usarlo
 CREATE SCHEMA IF NOT EXISTS trabajo
   DEFAULT CHARACTER SET utf8
-  COLLATE utf8_spanish2_ci;
+  COLLATE utf8_bin;
 USE trabajo;
 
 -- 2. Tablas principales
 
-CREATE TABLE JUGADOR (
-    nombre_jugador VARCHAR(60) NOT NULL,
+CREATE TABLE jugador (
+    nombre_jugador VARCHAR(80) NOT NULL,
     PRIMARY KEY (nombre_jugador)
 );
 
-CREATE TABLE JUEGO (
-    nombre_juego VARCHAR(60) NOT NULL,
+CREATE TABLE juego (
+    nombre_juego VARCHAR(80) NOT NULL,
     PRIMARY KEY (nombre_juego)
 );
 
-CREATE TABLE COPIA_JUEGO (
-    id_juego INT NOT NULL AUTO_INCREMENT,
-    nombre_juego VARCHAR(60) NOT NULL,
-    PRIMARY KEY (id_juego, nombre_juego),
-    FOREIGN KEY (nombre_juego)
-      REFERENCES JUEGO(nombre_juego)
-      ON DELETE RESTRICT
-      ON UPDATE CASCADE
-);
-
-CREATE TABLE EXPANSION (
-    nombre_exp VARCHAR(60) NOT NULL,
-    nombre_juego VARCHAR(60) NOT NULL,
-    PRIMARY KEY (nombre_exp),
-    FOREIGN KEY (nombre_juego)
-      REFERENCES JUEGO(nombre_juego)
-      ON DELETE RESTRICT
-      ON UPDATE CASCADE
-);
-
-CREATE TABLE COPIA_EXPANSION (
-    id_exp INT NOT NULL AUTO_INCREMENT,
-    nombre_exp VARCHAR(60) NOT NULL,
-    precio INT NOT NULL,
-    PRIMARY KEY (id_exp, nombre_exp),
-    FOREIGN KEY (nombre_exp)
-      REFERENCES EXPANSION(nombre_exp)
-      ON DELETE RESTRICT
-      ON UPDATE CASCADE
-);
-
-CREATE TABLE PERSONAL (
-    nombre_personal VARCHAR(60) NOT NULL,
+CREATE TABLE personal (
+    nombre_personal VARCHAR(80) NOT NULL,
     PRIMARY KEY (nombre_personal)
 );
 
--- 3. Partidas y relaciones con copias de juego
-
-CREATE TABLE PARTIDA (
-    id_partida INT NOT NULL AUTO_INCREMENT,
-    id_juego     INT  NOT NULL,
-    nombre_juego VARCHAR(60) NOT NULL,
-    PRIMARY KEY (id_partida),
-    -- FK compuesta hacia COPIA_JUEGO(id_juego, nombre_juego)
-    FOREIGN KEY (id_juego, nombre_juego)
-      REFERENCES COPIA_JUEGO(id_juego, nombre_juego)
+CREATE TABLE expansion (
+    nombre_expansion VARCHAR(80) NOT NULL,
+    nombre_juego VARCHAR(80) NOT NULL,
+    PRIMARY KEY (nombre_expansion),
+    FOREIGN KEY (nombre_juego)
+      REFERENCES juego(nombre_juego)
       ON DELETE RESTRICT
       ON UPDATE CASCADE
 );
 
-CREATE TABLE JUEGA (
-    nombre_jugador VARCHAR(60) NOT NULL,
+CREATE TABLE copia_juego (
+    id_juego INT NOT NULL,
+    nombre_juego VARCHAR(80) NOT NULL,
+    coste_juego INT NOT NULL,
+    PRIMARY KEY (id_juego, nombre_juego),
+    FOREIGN KEY (nombre_juego)
+      REFERENCES juego(nombre_juego)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE copia_expansion (
+    id_expansion INT NOT NULL,
+    nombre_expansion VARCHAR(80) NOT NULL,
+    coste_expansion INT NOT NULL,
+    PRIMARY KEY (id_expansion, nombre_expansion),
+    FOREIGN KEY (nombre_expansion)
+      REFERENCES expansion(nombre_expansion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+);
+
+
+-- 3. Partidas y relaciones con copias de juego
+
+CREATE TABLE partida (
+    id_partida INT NOT NULL AUTO_INCREMENT,
+    id_juego     INT  NOT NULL,
+    nombre_juego VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id_partida),
+    -- FK compuesta hacia COPIA_JUEGO(id_juego, nombre_juego)
+    FOREIGN KEY (id_juego, nombre_juego)
+      REFERENCES copia_juego(id_juego, nombre_juego)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+);
+
+CREATE TABLE juega (
+    nombre_jugador VARCHAR(80) NOT NULL,
     id_partida     INT  NOT NULL,
     PRIMARY KEY (nombre_jugador, id_partida),
     FOREIGN KEY (nombre_jugador)
-      REFERENCES JUGADOR(nombre_jugador)
+      REFERENCES jugador(nombre_jugador)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
     FOREIGN KEY (id_partida)
-      REFERENCES PARTIDA(id_partida)
+      REFERENCES partida(id_partida)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 );
 
-CREATE TABLE GANA (
-    nombre_jugador VARCHAR(60) NOT NULL,
+CREATE TABLE gana (
+    nombre_jugador VARCHAR(80) NOT NULL,
     id_partida     INT  NOT NULL,
     PRIMARY KEY (nombre_jugador, id_partida),
     FOREIGN KEY (nombre_jugador)
-      REFERENCES JUGADOR(nombre_jugador)
+      REFERENCES jugador(nombre_jugador)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
     FOREIGN KEY (id_partida)
-      REFERENCES PARTIDA(id_partida)
+      REFERENCES partida(id_partida)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 );
 
-CREATE TABLE ORGANIZA (
-    nombre_personal VARCHAR(60) NOT NULL,
+CREATE TABLE organiza (
+    nombre_personal VARCHAR(80) NOT NULL,
     id_partida      INT  NOT NULL,
     fecha           DATE NOT NULL,
     hora            TIME NOT NULL,
     PRIMARY KEY (nombre_personal, id_partida),
     FOREIGN KEY (nombre_personal)
-      REFERENCES PERSONAL(nombre_personal)
+      REFERENCES personal(nombre_personal)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
     FOREIGN KEY (id_partida)
-      REFERENCES PARTIDA(id_partida)
+      REFERENCES partida(id_partida)
       ON DELETE CASCADE
       ON UPDATE CASCADE
 );
 
 -- 4. Uso de expansiones en cada partida
 
-CREATE TABLE UTILIZA (
+CREATE TABLE utiliza (
     id_partida INT NOT NULL,
-    id_exp     INT NOT NULL,
-    nombre_exp VARCHAR(60) NOT NULL,
-    PRIMARY KEY (id_partida, id_exp, nombre_exp),
+    id_expansion     INT NOT NULL,
+    nombre_expansion VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id_partida, id_expansion, nombre_expansion),
     FOREIGN KEY (id_partida)
-      REFERENCES PARTIDA(id_partida)
+      REFERENCES partida(id_partida)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
-    FOREIGN KEY (id_exp, nombre_exp)
-      REFERENCES COPIA_EXPANSION(id_exp, nombre_exp)
+    FOREIGN KEY (id_expansion, nombre_expansion)
+      REFERENCES copia_expansion(id_expansion, nombre_expansion)
       ON DELETE RESTRICT
       ON UPDATE CASCADE
 );
